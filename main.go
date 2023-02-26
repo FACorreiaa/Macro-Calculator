94866c8273d50959df90572d345b30aa43cff994
fc <fc@gsmk.de>
2023-02-26 20:19:28 +0100

    update original source of info

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func main() {
+	//info and formula from:
+	//https://prophysiquemacros.com/
+
 	//creatre struct for values
 	//kg to lb and lb to kg values
 	//get formulas with values
