15bfc270a873b0ade7a1f2544ee9fc88ba0be7ad
fc <fc@gsmk.de>
2023-03-02 22:54:34 +0100

    create packahe for plan

---
+++ pkg/plan/main.go
@@ -0,0 +1,10 @@
+package goals
+
+import (
+	"FACorreiaa/Macro-Calculator/pkg/tdee"
+)
+
+func CalculateMacros() (float64, float64, float64) {
+	var tdeeValue = tdee.CalculateTdee()
+	println(tdeeValue)
+}
