9e5599e119a41d94a926be5bf2077e905cd3a957
fc <fc@gsmk.de>
2023-03-13 16:17:45 +0100

    delete annoying asterics

--- main.go
+++ main.go
@@ -19,23 +19,19 @@ func main() {
 	goal := goals.GetGoal(tdee)
 	macros := plan.CalculateMacroNutrients(goal)
 	fmt.Println(magenta("****************************************************************************"))
-	fmt.Println(orange("****************************** Inspired by:  *******************************"))
-	fmt.Println(magenta("************************* https://prophysiquemacros.com/ ******************"))
-	fmt.Println(orange("************************** Using the formula: ******************************"))
-	fmt.Println(magenta("************************** Mifflin-St Jeor Equation ************************"))
-	fmt.Println(orange("***************************** Give it a try ********************************"))
-	fmt.Println(magenta("****************************** Calculate your macros ***********************"))
+	fmt.Println(orange("Inspired by:  https://prophysiquemacros.com/"))
+	fmt.Println(magenta("Using the formula: Mifflin-St Jeor Equation"))
 
-	log.Printf("\n******** Age: %.0f Height:%.0f %s Weight:%.1f %s Gender: %s******** \n",
+	log.Printf("\nAge: %.0f Height:%.0f %s Weight:%.1f %s Gender: %s \n",
 		UserData.Age,
 		UserData.Height, red(Unit.Height),
 		UserData.Weight, red(Unit.Weight),
 		UserData.Gender)
-	fmt.Printf("***************** Your TDEE is %.2f *****************\n", tdee)
-	fmt.Printf("***************** Your goal is %.2f *****************\n", goal)
-	fmt.Printf("***************** Your activity Description %s *****************\n", red(activityDescription))
-	fmt.Print(magenta("***************** Based on your TDEE and your goal, your macro distributions are: *****************\n"))
-	fmt.Printf("\n***************** Protein %.2f: Fats:  %.2f Carbs %.2f *****************\n", macros.Protein, macros.Fats, macros.Carbs)
-	fmt.Println(orange("****************************************************************************"))
+	fmt.Printf("Your TDEE is %.2f\n", tdee)
+	fmt.Printf("Your goal is %.2f\n", goal)
+	fmt.Printf("Your activity Description %s\n", red(activityDescription))
+	fmt.Print(orange("Based on your TDEE and your goal, your macro distributions are: \n"))
+	fmt.Printf("\n Protein %.2f: Fats:  %.2f Carbs %.2f \n", macros.Protein, macros.Fats, macros.Carbs)
+	fmt.Println(magenta("****************************************************************************"))
 
 }
